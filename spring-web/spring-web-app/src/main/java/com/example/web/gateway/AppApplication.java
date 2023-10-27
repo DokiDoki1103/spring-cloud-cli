08b171ea5a834c01eab28065720d704fc0451609
逆流而上 <1666888816@qq.com>
2023-10-27 15:36:39 +0800

    增加图书模块

--- spring-web/spring-web-app/src/main/java/com/example/web/gateway/AppApplication.java
+++ spring-web/spring-web-app/src/main/java/com/example/web/gateway/AppApplication.java
@@ -2,7 +2,9 @@ package com.example.web.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients({"com.example.web.gateway.mapper"})
 @SpringBootApplication
 public class AppApplication {
     public static void main(String[] args) {
--- spring-web/spring-web-app/src/main/java/com/example/web/gateway/controller/ApiController.java
+++ spring-web/spring-web-app/src/main/java/com/example/web/gateway/controller/ApiController.java
@@ -1,6 +1,7 @@
 package com.example.web.gateway.controller;
 
 import com.example.web.gateway.mapper.RemoteProviderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +21,7 @@ public class ApiController {
         return "Hello, app-service!";
     }
 
-    @Resource
+    @Autowired
     private RemoteProviderService remoteProviderService;
 
     @GetMapping("/consumer")
